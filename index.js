a74fe7eabaf27ea5d209d5323fc642a9e0121f1d
François-Xavier Thoorens <fx.thoorens@gmail.com>
2023-05-15 16:44:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { PeerjsChannel } from "./src/peerjsChannel.js";
+
+export default {
+  PeerjsChannel
+}
---
+++ src/peerjsChannel.js
@@ -0,0 +1,100 @@
+
+import peerjs from 'peerjs';
+import { randomBytes, hash } from "../lib/vaultysid/src/crypto.js";
+import CryptoChannel from "../lib/vaultysid/src/cryptoChannel.js";
+
+const Peer = peerjs.Peer ? peerjs.Peer : peerjs;
+
+class PeerjsChannel {
+  constructor(key, status, host = "peerjs.92k.de", polyfills = null) {
+    this.host = host;
+    if (status) {
+      this.status = status;
+    } else {
+      this.status = key ? "receiver" : "initiator";
+    }
+    this.otherstatus = (this.status == "initiator") ? "receiver" : "initiator";
+    this.key = key ? key : randomBytes(32).toString("hex");
+    this.id = hash("sha256", Buffer.from(`vaultys-${this.status}-${this.key}`)).toString("hex");
+    this.otherid = hash("sha256", Buffer.from(`vaultys-${this.otherstatus}-${this.key}`)).toString("hex");
+    const options = { polyfills, debug: 2 };
+    options.host = this.host;
+    options.secure = true;
+    this.peer = new Peer(this.id, options);
+    if (this.status == "initiator") {
+      const that = this;
+      that.peer.on("connection", (conn) => {
+        console.log("connection");
+        that.conn = conn;
+        that.conn.on("data", (data) => {
+          //console.log("data", data);
+          that._onData(data);
+        });
+        that.conn.on("close", () => that.peer.destroy());
+        //console.log("_onStarted call", that);
+        that._onStarted();
+      });
+    }
+    CryptoChannel.encryptChannel(this, Buffer.from(this.key, "hex"));
+  }
+
+  static fromConnectionString(string) {
+    if(string.startsWith('vaultys://peerjs?')) {
+      const url = new URL(string);
+      const key = url.searchParams.get('key');
+      const host = url.searchParams.get('host');
+      return new PeerjsChannel(key, "receiver", host);
+    }
+    return null;
+  }
+
+  getConnectionString() {
+    return `vaultys://peerjs?key=${this.key}&host=${this.host}`;
+  }
+
+  async start(onConnect) {
+    const that = this;
+    console.log(that);
+    if (this.status === "receiver") {
+      if(onConnect) {
+        if(!await onConnect()) {
+          this.close();
+          return;
+        }
+      } else {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
+      this.conn = this.peer.connect(this.otherid);
+      that.conn.on("open", (id) => {
+        console.log("open");
+        that.conn.on("data", (data) => {
+          //console.log("data", data);
+          that._onData(data);
+        });
+        that.conn.on("close", () => that.peer.destroy());
+        //console.log("_onStarted call", that);
+        that._onStarted();
+      });
+    }
+    return new Promise((resolve) => (that._onStarted = resolve));
+  }
+
+  send(data) {
+    this.conn.send(data);
+  }
+
+  async receive() {
+    const that = this;
+    return new Promise((resolve) => {
+      that._onData = resolve;
+    });
+  }
+
+  close() {
+    if (this.conn) {
+      this.conn.close();
+    }
+    this.peer.destroy();
+  }
+}
+export default PeerjsChannel;
